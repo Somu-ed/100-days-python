366b39414e7441b307429d5a5d0cdb63a3952322
Soumyajeet Satpathy <satpathysoumyajeet.99@gmail.com>
2024-08-23 01:11:11 +0530

    day 001

---
+++ Day001/main.py
@@ -0,0 +1,42 @@
+# print statement
+print("Well we")
+print("just use more lines")
+print("of code")
+
+# This is how you can print a big block of text with gaps and line breaks in it.
+print("""Anything that starts
+with three quotes, and ends
+in three quotes can span
+many lines and even contain " symbols
+within it without freaking anything out!""")
+
+# ------------------------------------------------
+
+## Fix my code!
+# print(= MUSIC+ =)
+# print("> Songs"
+# Print("> Albums")
+# print(> Artists")
+
+# Answer
+print("= MUSIC+ =")
+print("> Songs")
+print("> Albums")
+print("> Artists")
+
+# ------------------------------------------------
+
+## Day 1 Challenge:
+
+# 1
+print("Soumyajeet Satpathy")
+print("22 August, 2024")
+# 2
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+# 3
+print("I am feeling \U0001f61C")
+# 4
+print("You can follow my progress at replit.com/@Somued")
+# 5 - Program run successfully!
